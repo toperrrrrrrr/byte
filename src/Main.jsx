4eaa103af3732ad65586cd8e5a0f90f66c8fa146
Nori Lacsina <toperrrrrrrr@gmail.com>
2024-05-30 20:29:54 +0800

    Working on Dashboard Layout.

--- src/Main.jsx
+++ src/Main.jsx
@@ -11,50 +11,50 @@ export default function Main() {
                   <div className="row vh-100">
                      <Sidebar />
                      <div className="col-10 dashboard">
-                        <div class="container ">
+                        <div className="container ">
                            <p>Hello Admin, welcome back!</p>
                            <h1>Your dashboard is updated</h1>
                         </div>
-                        <div class="container mt-3">
-                           <div className="row visualize gx-5 vh-100">
-                              <div className="col-4 colorize2 ">
-                                 <div class="p-3 border bg-dark vh-100">
-                                    Custom column padding
-                                 </div>
-                              </div>
-                              <div className="col-8 colorize2">
-                                 <div className="row visualize gx-5 ">
-                                    <div className="col-4 colorize1">
-                                       <div class="p-3 border bg-dark">
+                        <div className="container mt-3">
+                           <div className="row  gx-5 vh-100">
+                              <div className="col-8 ">
+                                 <div className="row g-5 transparent">
+                                    <div className="col-4 ">
+                                       <div className="p-3 border bg-dark">
                                           Custom column padding
                                        </div>
                                     </div>
-                                    <div className="col-4 colorize2">
-                                       <div class="p-3 border bg-dark">
+                                    <div className="col-4 ">
+                                       <div className="p-3 border bg-dark">
                                           Custom column padding
                                        </div>
                                     </div>
-                                    <div className="col-4 colorize3">
-                                       <div class="p-3 border bg-dark">
+                                    <div className="col-4 ">
+                                       <div className="p-3 border bg-dark">
                                           Custom column padding
                                        </div>
                                     </div>
-                                    <div className="col-6 colorize1 ">
-                                       <div class="p-3 border bg-dark">
+                                    <div className="col-6  ">
+                                       <div className="p-3 border bg-dark">
                                           Custom column padding
                                        </div>
                                     </div>
 
-                                    <div className="col-3 colorize3 ">
-                                       <div class="p-3 border bg-dark">
+                                    <div className="col-3  ">
+                                       <div className="p-3 border bg-dark">
                                           Custom column padding
                                        </div>
                                     </div>
-                                    <div className="col-3 colorize2 ">
-                                       <div class="p-3 border bg-dark">
+                                    <div className="col-3  ">
+                                       <div className="p-3 border bg-dark">
                                           Custom column padding
                                        </div>
                                     </div>
+                                 </div>
+                              </div>
+                              <div className="col-4  ">
+                                 <div className="p-3 border bg-dark vh-100">
+                                    Custom column padding
                                  </div>
                               </div>
                            </div>
