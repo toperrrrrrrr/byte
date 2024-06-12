83a152398d785631fc997b64789bea2c7b3248d5
Nori Lacsina <toperrrrrrrr@gmail.com>
2024-06-12 21:20:00 +0800

    working on dashboard

--- src/BackOffice/MainContainer.jsx
+++ src/BackOffice/MainContainer.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import "../res/css/App.css";
 
 export default function MainContainer() {
-  return (
-    <div>
-      <h1>hello</h1>
-    </div>
-  )
+   return (
+      <div className="vh-100">
+         <div className="container mw-100 ">
+            <div className="row">
+               <div className="col-md-1 colorize1">
+                  <h1>hello</h1>
+                  <h1>hello</h1>
+                  <h1>hello</h1>
+                  <h1>hello</h1>
+                  <h1>hello</h1>
+                  <h1>hello</h1>
+                  <h1>hello</h1>
+                  <h1>hello</h1>
+                  <h1>hello</h1>
+                  <h1>hello</h1>
+               </div>
+               <div className="col-md-11 colorize1 ">
+                  <h1>hi</h1>
+               </div>
+            </div>
+         </div>
+      </div>
+   );
 }
