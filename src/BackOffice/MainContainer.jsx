9d62bca8ac813563b0c0cc767dcb9662318168c1
Nori Lacsina <toperrrrrrrr@gmail.com>
2024-06-13 21:53:31 +0800

    Started working on creating the product lines

--- src/BackOffice/MainContainer.jsx
+++ src/BackOffice/MainContainer.jsx
@@ -6,20 +6,40 @@ export default function MainContainer() {
       <div className="vh-100">
          <div className="container mw-100 ">
             <div className="row">
-               <div className="col-md-1 colorize1">
-                  <h1>hello</h1>
-                  <h1>hello</h1>
-                  <h1>hello</h1>
-                  <h1>hello</h1>
-                  <h1>hello</h1>
-                  <h1>hello</h1>
-                  <h1>hello</h1>
-                  <h1>hello</h1>
-                  <h1>hello</h1>
-                  <h1>hello</h1>
-               </div>
+               <div className="col-md-1 colorize1"></div>
                <div className="col-md-11 colorize1 ">
-                  <h1>hi</h1>
+                  <form className="wrap-login p-5">
+                     <span className="signin-title "> Product line </span>
+                     <div className="wrap-input w-100">
+                        <span>Item name</span>
+                        <input className="inputs" type="text" required />
+                     </div>
+                     <div className="wrap-input w-100">
+                        <span>Description</span>
+                        <input className="inputs" type="text" required />
+                     </div>
+                     <div className="wrap-input w-100">
+                        <span>Display as</span>
+                        <input className="inputs" type="text" required />
+                     </div>
+                     <div className="wrap-input w-100">
+                        <span>Price</span>
+                        <input className="inputs" type="text" required />
+                     </div>
+                     <div className="wrap-input w-100">
+                        <span>Category</span>
+                        <input className="inputs" type="text" required />
+                     </div>
+                     <div className="wrap-input w-100">
+                        <span>Course</span>
+                        <input className="inputs" type="text" required />
+                     </div>
+                     <div className="btn-wrap">
+                        <button className="btn-login" type="submit">
+                           Register
+                        </button>
+                     </div>
+                  </form>
                </div>
             </div>
          </div>
