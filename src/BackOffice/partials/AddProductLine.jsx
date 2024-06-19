35361648de8cdd19b599c8f31416cc88a7edd562
Nori Lacsina <toperrrrrrrr@gmail.com>
2024-06-19 19:04:34 +0800

    Working on delete function for product lines

--- src/BackOffice/partials/AddProductLine.jsx
+++ src/BackOffice/partials/AddProductLine.jsx
@@ -21,81 +21,66 @@ export default function AddProductLine() {
       }
    };
    return (
-      <div>
-         <div className="col-lg-5">
-            <div className="card">
-               <div className="card-body">
-                  <h5 className="card-title">Add new products</h5>
-                  <form>
-                     <div className="row mb-3">
-                        <label
-                           for="inputText"
-                           className="col-sm-3 col-form-label"
+      <div className="col-lg-5">
+         <div className="card">
+            <div className="card-body">
+               <h5 className="card-title">Add new products</h5>
+               <form>
+                  <div className="row mb-3">
+                     <label className="col-sm-3 col-form-label">Name</label>
+                     <div className="col-sm-9">
+                        <input
+                           type="text"
+                           className="form-control"
+                           onChange={(e) => setName(e.target.value)}
+                        />
+                     </div>
+                  </div>
+                  <div className="row mb-3">
+                     <label className="col-sm-3 col-form-label">Price</label>
+                     <div className="col-sm-9">
+                        <input
+                           type="text"
+                           className="form-control"
+                           onChange={(e) => setPrice(e.target.value)}
+                        />
+                     </div>
+                  </div>
+                  <div className="row mb-3">
+                     <label className="col-sm-3 col-form-label">
+                        Description
+                     </label>
+                     <div className="col-sm-9">
+                        <textarea
+                           className="form-control"
+                           onChange={(e) => setDesc(e.target.value)}
+                        ></textarea>
+                     </div>
+                  </div>
+                  <div className="row mb-3">
+                     <label className="col-sm-3 col-form-label">
+                        Display As
+                     </label>
+                     <div className="col-sm-9">
+                        <textarea
+                           className="form-control"
+                           onChange={(e) => setDisplayAs(e.target.value)}
+                        ></textarea>
+                     </div>
+                  </div>
+                  <div className="row mb-3">
+                     <label className="col-sm-3 col-form-label"></label>
+                     <div className="col-sm-9">
+                        <button
+                           onClick={handleSubmit}
+                           className="btn btn-primary"
+                           type="submit"
                         >
-                           Name
-                        </label>
-                        <div className="col-sm-9">
-                           <input
-                              type="text"
-                              className="form-control"
-                              onChange={(e) => setName(e.target.value)}
-                           />
-                        </div>
+                           Add Item
+                        </button>
                      </div>
-                     <div className="row mb-3">
-                        <label
-                           for="inputText"
-                           className="col-sm-3 col-form-label"
-                        >
-                           Price
-                        </label>
-                        <div className="col-sm-9">
-                           <input
-                              type="text"
-                              className="form-control"
-                              onChange={(e) => setPrice(e.target.value)}
-                           />
-                        </div>
-                     </div>
-                     <div className="row mb-3">
-                        <label
-                           for="inputPassword"
-                           className="col-sm-3 col-form-label"
-                        >
-                           Description
-                        </label>
-                        <div className="col-sm-9">
-                           <textarea
-                              className="form-control"
-                              onChange={(e) => setDesc(e.target.value)}
-                           ></textarea>
-                        </div>
-                     </div>
-                     <div className="row mb-3">
-                        <label className="col-sm-3 col-form-label">
-                           Display As
-                        </label>
-                        <div className="col-sm-9">
-                           <textarea
-                              className="form-control"
-                              onChange={(e) => setDisplayAs(e.target.value)}
-                           ></textarea>
-                        </div>
-                     </div>{" "}
-                     <div className="row mb-3">
-                        <label className="col-sm-3 col-form-label"></label>
-                        <div className="col-sm-9">
-                           <button
-                              onClick={handleSubmit}
-                              className="btn btn-primary"
-                              type="button"
-                           >
-                              Add Item
-                           </button>
-                        </div>
-                     </div>
-                  </form>
-               </div>
+                  </div>
+               </form>
             </div>
          </div>
       </div>
